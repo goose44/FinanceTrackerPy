524cde410766d61cbc76c7435099c991bbde8d9f
johnn <jn150679@gmail.com>
2026-07-14 15:03:57 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+"My personal finance tracker"
+
+print("Is the program running?")
+
+def finance ():
+
+    print("Welcome to my finance tracker!")
+
+    balance = float(input("Enter your balance: "))
+    income = float(input("Enter your monthly income: "))
+    expense = float(input("Enter your monthly expense: "))
+
+    print("Income:", income)
+    print("Expense:", expense)
+    print("Remaining Balance:", balance)
+
+finance()
