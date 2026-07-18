30f08e737ece39c11f3da5bffb989e56f70c414d
johnn <jn150679@gmail.com>
2026-07-18 15:57:01 -0500

    organized functions and expense tracking

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
 "My personal finance tracker"
 
-def get_starting_info():
+import json
+import os
 
+def save_expenses(expenses):
+    with open("expenses.json", "w") as file:
+        json.dump(expenses, file, indent=4)
+
+def load_expenses():
+    if os.path.exists("expenses.json"):
+
+        with open("expenses.json", "r") as file:
+            return json.load(file)
+    return {}
+
+def get_starting_info():
     starting_balance = float(input("Enter your balance: "))
     income = float(input("Enter your monthly income: "))
 
     return starting_balance, income
 
-def get_expenses():
-
-    expenses = {}
+def get_expenses(expenses):
 
     while True:
         category = str(input("Enter expense category (or 'done' to finish): "))
@@ -22,14 +33,12 @@ def get_expenses():
     return expenses
 
 def calculate_balance(starting_balance, income, expenses):
-
     total = sum(expenses.values())
     ending_balance = starting_balance + income - total
 
     return total, ending_balance
 
 def display_summary(starting_balance, income, expenses, total, ending_balance):
-
     print("\n------ Summary ------")
 
     print(f"Starting Balance: ${starting_balance}")
@@ -44,15 +53,13 @@ def display_summary(starting_balance, income, expenses, total, ending_balance):
     print(f"Remaining Balance: ${ending_balance}")
 
 def finance ():
-
     print("Welcome to my finance tracker!")
 
     starting_balance, income = get_starting_info()
-
-    expenses = get_expenses()
-
+    expenses = load_expenses()
+    expenses = get_expenses(expenses)
     total, ending_balance = calculate_balance(starting_balance, income, expenses)
-
     display_summary(starting_balance, income, expenses, total, ending_balance)
+    save_expenses(expenses)
 
 finance()
